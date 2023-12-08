20f51c9eb3780c92c68ca75c062d548b3f1e66f5
Mohammad Didarul Alam <emdidar@gmail.com>
2023-12-09 00:36:21 +0600

    Create Assignment.js

---
+++ js/components/Assignment.js
@@ -0,0 +1,15 @@
+export default{
+  template: `
+    <li>
+      <label class="p-2 flex justify-between items-center">
+        {{ assignment.name }}
+        <input type="checkbox" v-model="assignment.complete" class="ml-4">
+      </label>
+    </li>
+  `,
+
+  props:{
+    assignment: Object
+  }
+
+}
