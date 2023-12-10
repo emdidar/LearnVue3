749edeaa494b16ba92015249d895611e9dc35dc8
Mohammad Didarul Alam <emdidar@gmail.com>
2023-12-11 02:00:11 +0600

    Update AssignmentList.js

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,26 +1,28 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 export default {
 	components: {
-		Assignment
+		Assignment, AssignmentTags
 	},
 
 	template: `
-        <section v-show="assignments.length" >
+        <section v-show="assignments.length" class="w-60" >
             <h2 class="font-bold mb-2">
             {{ title }} 
             <span>({{ assignments.length }})</span>
             </h2>
 
-            <div class="flex gap-2">
-            <button @click="currentTag=tag" v-for="tag in tags" class="border rounded px-1 py-px text-xs">{{ tag }}</button>
-            </div>
+			<assignment-tags 
+                v-model:currentTag="currentTag" 
+				:initial-tags="assignments.map(a => a.tag)" 
+			/>
 
             <ul class="border border-gray-600 divide-y mt-4">
-            <assignment 
-                v-for="assignment in filterAssignments" 
-                :key="assignment.id" 
-                :assignment="assignment"
-            ></assignment>
+				<assignment 
+					v-for="assignment in filterAssignments" 
+					:key="assignment.id" 
+					:assignment="assignment"
+				></assignment>
             </ul>  
         </section>
     `,
@@ -43,9 +45,7 @@ export default {
                 return this.assignments;
             }
             return this.assignments.filter(a=> a.tag===this.currentTag);
-        },
-		tags() {
-			return ['All',...new Set(this.assignments.map(a => a.tag))];
-		}
+        }
+		
 	}
 };
