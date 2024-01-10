28c71723d92d98e32864c664b84a5d09298a4ba6
Mohammad Didarul Alam <emdidar@gmail.com>
2024-01-10 23:57:15 +0600

    Update App.vue

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
-  const message="Welcome. to Vue JS";
-  function getDate(){
-    const date=new Date();
-    return date.toDateString();
-  }
+  //import {data} from './data/data.js'
+  import data from './data/data'
 </script>
 
 <template>
   <div class="max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-    <h1 class="text-4xl">{{ message }}</h1>
-    <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ getDate() }}</span></h2>
+    <h1 class="text-4xl">{{ data.message }}</h1>
+    <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ data.getDate }}</span></h2>
+    <p class="mt-10 text-xl text-gray-700">I have <span>{{ data.tasks.length }}</span> for Today</p>
   </div>
 </template>
 
